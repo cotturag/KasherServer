b6f5e697d72cf6f3c5deb18bf6bedc3bcec02670
cotturag <cotturag@gmail.com>
2022-03-18 18:29:55 +0100

    működik az adatbázis szinkronizálása ebben az állásban, a user és a privilege táblákkal még van dolog

--- access.php
+++ access.php
@@ -1,14 +1,90 @@
 <?php
-
 session_start();
+include("dbConnectRedbean.php");
 
 $json= file_get_contents("php://input");
 $data= json_decode($json,true);
-//$text = $data["name"];
-foreach ($data as $key => $value){
-    $cn=$key." ".$value;
+
+$method=$data["method"];
+unset($data["method"]);
+
+if ($method=="insert"){
+    $idfd=$data["id"];
+    unset($data["id"]);
+    $funds=R::dispense('funds');
+    foreach ($data as $key => $value){
+        $funds -> $key = $value;
+    }
+    $funds -> idfd = $idfd;
+    R::store($funds);
+    
+    
+}
+if ($method=="update"){
+    $funds=R::dispense('funds');
+    $idfd=$data["id"];
+    unset($data["id"]);
+    $family=$data["family"];
+    $sql="SELECT id FROM funds WHERE idfd=".$idfd." AND family='".$family."'";    
+    $content=R::getAll($sql);
+    $res=0;
+    foreach ($content[0] as $k => $v){
+        $res=$v;    
+    }   
+    foreach ($data as $key => $value){
+        $funds -> $key = $value;
+    }
+    $funds -> idfd = $idfd;
+    $funds -> id = $res;
+    R::store($funds);
+    
+    
+    
+}
+if ($method=="delete"){
+    $funds=R::dispense('funds');
+    $idfd=$data["id"];
+    unset($data["id"]);
+    $family=$data["family"];
+    $sql="SELECT id FROM funds WHERE idfd=".$idfd." AND family='".$family."'";    
+    $content=R::getAll($sql);
+    $res=0;
+    foreach ($content[0] as $k => $v){
+        $res=$v;    
+    }   
+    foreach ($data as $key => $value){
+        $funds -> $key = $value;
+    }
+    $funds -> idfd = $idfd;
+    $funds -> id = $res;
+    R::trash($funds);
+    
+    
 }
 
 
 
-file_put_contents ('files/test.txt', $cn);
+foreach ($data as $key => $value){
+    $cn=$key." ".$value."\n";
+    $cnk=$cnk.$cn;
+}
+
+/*
+foreach ($funds as $key => $value){
+    $cn=$key." ".$value."\n";
+    $cnk=$cnk.$cn;
+}
+ 
+ */
+file_put_contents ('files/test.txt', $cnk);
+
+
+
+
+
+
+
+
+
+
+
