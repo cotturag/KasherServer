abfe7b18e89c66cb4501871b0c58caafc52930c1
cotturag <cotturag@gmail.com>
2022-04-01 14:47:46 +0200

    tranzakcióknál működik a beszúrás

--- index.php
+++ index.php
@@ -1,27 +1,29 @@
 <?php 
-include("db_connect.php");
+
 session_start();
 
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-*/
-$fh = fopen('files/test.txt','r');
-while ($line = fgets($fh)) { 
-   echo($line);
-}
-fclose($fh);
+
+
+include("dbConnectRedbean.php");
 
 
 
-
-$sql="select * from users";
-$content= mysqli_query($conn,$sql);
-if (mysqli_num_rows($content)>0){
-    while ($row= mysqli_fetch_assoc($content)){
-        echo strval($row["id"]);
-    }
+$sql="SELECT * FROM transactions";
+$content=R::getAll($sql);
     
+foreach ($content as $k => $v)   {
+    foreach ($v as $key => $value){
+        echo $value." | ";
+    }
+    echo "<br>";
 }
 
+/*
+$sql="DELETE * FROM funds";
+        R::exec($sql);
+ 
+ */
 ?>
